8dd883947dd1f88b066f645a47b03bac60cfd68d
kristinadd <kristina@productboard.com>
2024-12-07 13:05:17 +0100

    Day 1 Part 2

---
+++ Day_1/historian_hysteria_part_two.rb
@@ -0,0 +1,31 @@
+require 'csv'
+
+class HistorianHysteriaPartTwo
+  FILE_PATH = 'Day_1/day_1.csv'.freeze
+
+  def self.process
+    left, right = CSV.foreach(FILE_PATH, headers: false).map do |row|
+      row[0].split.map(&:to_i)
+    end.transpose
+
+    occurrences = {}
+
+    right.each do |number|
+      if occurrences.include?(number)
+        occurrences[number] += 1
+      else
+        occurrences[number] = 1
+      end
+    end
+
+    score = 0
+
+    left.each do |number|
+     score +=  number * ( occurrences.include?(number) ? occurrences[number] : 0 )
+    end
+
+    puts score
+  end
+end
+
+HistorianHysteriaPartTwo.process
