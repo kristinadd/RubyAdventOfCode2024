b03aa231cc0549cec0e735ae0ee16a58dfa65321
kristinadd <kristina@productboard.com>
2024-12-21 20:37:36 +0100

    Testing

--- Day_2/red_nosed_reports.rb
+++ Day_2/red_nosed_reports.rb
@@ -1,23 +1,51 @@
 require 'csv'
 
 class RedNosedReports
-  FILE_PATH = "Day_2/day_2.csv".freeze
-
-  def self.parse_csv(path = FILE_PATH)
-    int_elements = []
-    CSV.foreach(path, headers: false) do |row|
-      int_elements.push(row[0].split(" ").map { |element| element.to_i })
+  def self.parse_csv(file_path)
+    int_reports = []
+    CSV.foreach(file_path, headers: false) do |row|
+      int_reports.push(row[0].split(" ").map { |element| element.to_i })
     end
-    return int_elements
+    return int_reports
   end
 
+  # def self.is_valid?(report)
+  #   puts report.inspect
+  #     report.each_with_index do |_, index|
+  #       if index < report.length - 1
+  #         difference = report[index] - report[index + 1]
+  #         if difference.abs > 3 || difference.abs < 1
+  #           puts 'Invalid report'
+  #           return false
+  #         else
+  #           puts "Valid report"
+  #           return true
+  #         end
+  #       end
+  #     end
+  # end
 
-  def self.valid_reports(int_elements)
-    puts int_elements.inspect
+  def self.is_valid?(report)
+    puts report.inspect
+    report.each_with_index do |_, index|
+      if index < report.length - 1
+        difference = report[index] - report[index + 1]
+        if difference.abs > 3 || difference.abs < 1
+          puts 'Invalid report'
+          return false
+        end
+      end
+    end
+    # If the loop completes without returning false, the report is valid
+    puts "Valid report"
+    return true
   end
+
 end
 
-int_elements = RedNosedReports.parse_csv(RedNosedReports::FILE_PATH)
-RedNosedReports.valid_reports(int_elements)
+int_reports = RedNosedReports.parse_csv("Day_2/day_2.csv")
+int_reports.each do |report|
+  RedNosedReports.is_valid?(report)
+end
 
-               ... TBC ...
+
