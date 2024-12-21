f42f216cacaa06c965b759042e296e1bf7ef7568
kristinadd <kristina@productboard.com>
2024-12-21 19:01:37 +0100

    Testing

--- Day_2/red_nosed_reports.rb
+++ Day_2/red_nosed_reports.rb
@@ -1,9 +1,23 @@
 require 'csv'
 
 class RedNosedReports
-  def self.process
-    
+  FILE_PATH = "Day_2/day_2.csv".freeze
+
+  def self.parse_csv(path = FILE_PATH)
+    int_elements = []
+    CSV.foreach(path, headers: false) do |row|
+      int_elements.push(row[0].split(" ").map { |element| element.to_i })
+    end
+    return int_elements
+  end
+
+
+  def self.valid_reports(int_elements)
+    puts int_elements.inspect
   end
 end
 
-RedNosedReports.process
+int_elements = RedNosedReports.parse_csv(RedNosedReports::FILE_PATH)
+RedNosedReports.valid_reports(int_elements)
+
+               ... TBC ...
