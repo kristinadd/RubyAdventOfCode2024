e4352f017f733229506d49b155c63e4666b6d08a
kristinadd <kristina@productboard.com>
2024-12-22 10:31:13 +0100

    Day 2 Part 1

--- Day_2/red_nosed_reports.rb
+++ Day_2/red_nosed_reports.rb
@@ -2,30 +2,14 @@ require 'csv'
 
 class RedNosedReports
   def self.parse_csv(file_path)
-    int_reports = []
+    reports = []
     CSV.foreach(file_path, headers: false) do |row|
-      int_reports.push(row[0].split(" ").map { |element| element.to_i })
+      reports.push(row[0].split(" ").map { |element| element.to_i })
     end
-    return int_reports
+    return reports
   end
 
-  # def self.is_valid?(report)
-  #   puts report.inspect
-  #     report.each_with_index do |_, index|
-  #       if index < report.length - 1
-  #         difference = report[index] - report[index + 1]
-  #         if difference.abs > 3 || difference.abs < 1
-  #           puts 'Invalid report'
-  #           return false
-  #         else
-  #           puts "Valid report"
-  #           return true
-  #         end
-  #       end
-  #     end
-  # end
-
-  def self.is_valid?(report)
+  def self.valid?(report)
     puts report.inspect
     report.each_with_index do |_, index|
       if index < report.length - 1
@@ -36,16 +20,39 @@ class RedNosedReports
         end
       end
     end
-    # If the loop completes without returning false, the report is valid
     puts "Valid report"
     return true
   end
 
+  def self.increasing?(report)
+    return false if report.length < 2
+    if report.each_cons(2).all? { |a, b | a < b && ((b - a) <= 3 && (b - a ) >= 1) }
+      puts "Increasing report"
+      return true
+    end
+    return false
+  end
+
+  def self.decreasing?(report)
+    return false if report.length < 2
+    if report.each_cons(2).all? { |a, b| a > b && ((a - b) <= 3 && (a - b) >= 1) }
+      puts "Decreasing report"
+      return true
+    end
+    return false
+  end
+
+  def self.count_safe_report(file_path)
+    reports = RedNosedReports.parse_csv(file_path)
+    safe_report = 0
+    reports.each do |report|
+      if RedNosedReports.valid?(report) && (RedNosedReports.increasing?(report) || RedNosedReports.decreasing?(report))
+        puts "All safe reports are:  #{safe_report += 1}"
+      end
+      false
+    end
+  end
 end
 
-int_reports = RedNosedReports.parse_csv("Day_2/day_2.csv")
-int_reports.each do |report|
-  RedNosedReports.is_valid?(report)
-end
-
+RedNosedReports.count_safe_report('Day_2/day_2.csv')
 
