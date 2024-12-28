97453dbc9f6247073476ecf9095e5fc21c89117f
kristinadd <kristina@productboard.com>
2024-12-28 14:04:54 +0100

    Day 3 Part 1

---
+++ Day_3/instruction_processor_2.rb
@@ -0,0 +1,40 @@
+require 'csv'
+
+class InstructionProcessor
+  def self.process_file(file_path)
+    new(file_path).process
+  end
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def process
+    read_csv_file
+      .then { |csv_content| parse_csv_file(csv_content) }
+      .then { |matches| convert_to_ints(matches) }
+      .then { |int_matches| sum_matches(int_matches) }
+  end
+
+  private
+
+  def read_csv_file
+    File.read(@file_path)
+  end
+
+  def parse_csv_file(csv_content)
+    csv_content.scan(/mul\((\d+),(\d+)\)/)
+  end
+
+  def convert_to_ints(matches)
+    matches.map { |match| match.map(&:to_i) }
+  end
+
+  def sum_matches(matches)
+    sum = matches.sum { |x, y| x * y }
+    puts "The total sum of the instructions is: #{sum}"
+  end
+end
+
+# Usage
+InstructionProcessor.process_file('Day_3/day_3.csv')
